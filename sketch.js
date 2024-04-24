acc553786f7c0e6b757cfd2e014021d6aa49b8bc
Jonas C Silva Jr <jonas.junior@bb.com.br>
2024-04-24 10:38:07 -0300

    Borders implementation

--- sketch.js
+++ sketch.js
@@ -1,13 +1,25 @@
-let ballX = 300;
-let ballY= 200;
-let ballDiameter = 50;
+let xBall = 300;
+let yBall= 200;
+let dBall = 15;
+let radiusBall = dBall / 2;
 
-
+let xBallVelocity = 2;
+let yBallVelocity = 2;
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
   background(0);
-  circle(ballX, ballY, ballDiameter);
+  circle(xBall, yBall, dBall);
+  xBall += xBallVelocity;
+  yBall += yBallVelocity;
+
+  if (xBall + radiusBall > width || xBall - radiusBall  < 0) { 
+    xBallVelocity *= -1;
+  }
+  if (yBall + radiusBall > height || yBall - radiusBall < 0) { 
+    yBallVelocity *= -1;
+  }
+  // console.log(height);
 }
