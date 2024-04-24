e541acd3a3c341f3be60937286d52a512c96ad97
Jonas Junior <jonas.developer@live.com>
2024-04-24 20:13:42 -0300

    add com racket

--- sketch.js
+++ sketch.js
@@ -1,14 +1,31 @@
+// import { collideRectCircle } from "p5collide";
+// Canvas
 let canvasWidth = 600;
 let canvasHeight = 400;
 
+// Ball settings
 let dBall = 15;
 let radiusBall = dBall / 2;
 let xBall = canvasWidth / 2;
 let yBall = canvasHeight / 2;
-
 let velocity = 6;
 let xBallVelocity = velocity;
 let yBallVelocity = velocity;
+
+let hit = false;
+
+//Racket settings
+let racketWidth = 10;
+let racketHeight = 90;
+// Player Settings
+let xPlayerRacket = 5;
+let yPlayerRacket = 400 / 2 - racketHeight / 2;
+let player_points = 0;
+//Com Settings
+let xComRacket = canvasWidth - racketWidth - 5;
+let yComRacket = yPlayerRacket;
+let com_points = 0;
+
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
 }
@@ -18,12 +35,15 @@ function draw() {
   drawBall();
   moveBall();
   ballBorderCollision();
+  drawRacket(xPlayerRacket, yPlayerRacket);
 
-  drawPlayerRacket();
   movePlayerRacket();
-  racketCollision();
+  racketCollisionLib();
 
-  debugLines();
+  drawRacket(xComRacket, yComRacket);
+  moveComRacket();
+
+  // debugLines();
 }
 
 function drawBall() {
@@ -36,38 +56,49 @@ function moveBall() {
 function ballBorderCollision() {
   if (xBall - radiusBall < 0) {
     xBallVelocity *= -1;
+    com_points += 1;
+    console.log(`Opponet Goal: ${com_points}`)
   }
   if (xBall + radiusBall > width) {
     xBallVelocity *= -1;
+    player_points += 1;
   }
   if (yBall + radiusBall > height || yBall - radiusBall < 0) {
     yBallVelocity *= -1;
   }
 }
 
-let racketWidth = 10;
-let racketHeight = 90;
-let xRacket = 5;
-let yRacket = 400 / 2 - racketHeight / 2;
-
-function drawPlayerRacket() {
-  rect(xRacket, yRacket, racketWidth, racketHeight);
+function drawRacket(x, y) {
+  rect(x, y, racketWidth, racketHeight);
 }
 
 function movePlayerRacket() {
   //TODO: add border limit
   if (keyIsDown(UP_ARROW)) {
-    yRacket -= 10;
+    yPlayerRacket -= 10;
   }
   if (keyIsDown(DOWN_ARROW)) {
-    yRacket += 10;
+    yPlayerRacket += 10;
   }
 }
-function racketCollision() {
-  if (xBall - radiusBall < xRacket + racketWidth / 2 && (yBall <= yRacket + racketHeight && yBall >= yRacket)) {
+// function racketCollision() {
+//   if (xBall - radiusBall < xPlayerRacket + racketWidth
+//     && yBall - radiusBall <= yPlayerRacket + racketHeight
+//     && yBall + radiusBall >= yPlayerRacket) {
+//     xBallVelocity *= -1;
+//   }
+// }
+function racketCollisionLib() {
+  hit = collideRectCircle(xPlayerRacket, yPlayerRacket, racketWidth, racketHeight, xBall, yBall, dBall);
+  if (hit) {
     xBallVelocity *= -1;
   }
+}
 
+let drift = 0;
+function moveComRacket() {
+  drift = yBall - yComRacket - racketHeight / 2 - 30; 
+  yComRacket += drift;
 }
 
 function debugLines() {
